02635816e6b8b2e1dca62c7d93d9bccbf80b5441
korea1501541730 <1501541730@qq.com>
2024-01-16 18:32:36 +0800

    no message

---
+++ flutter_desktop/lib/feature/mine/pages/language_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LanguagePage extends StatefulWidget {
+  const LanguagePage({super.key});
+
+  @override
+  State<LanguagePage> createState() => _LanguagePageState();
+}
+
+class _LanguagePageState extends State<LanguagePage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ flutter_desktop/lib/feature/mine/pages/theme_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ThemePage extends StatefulWidget {
+  const ThemePage({super.key});
+
+  @override
+  State<ThemePage> createState() => _ThemePageState();
+}
+
+class _ThemePageState extends State<ThemePage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
